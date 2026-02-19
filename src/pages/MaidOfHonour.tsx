3363f210f61fd1591b0629619fdd2f0e68e8a0f4
ayazhankadessova <kadessovaayazhan@gmail.com>
2026-02-19 10:17:14 +0300

    feat: personalize maid of honour page for Gulim

--- src/pages/MaidOfHonour.tsx
+++ src/pages/MaidOfHonour.tsx
@@ -57,7 +57,7 @@ const MaidOfHonour = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
           >
-            A Special Question
+            Dear Gulim
           </motion.p>
 
           <motion.h1
